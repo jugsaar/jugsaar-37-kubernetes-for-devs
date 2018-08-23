427cc56466b604bd5a47976c5fb424bebf473de7
Thomas Darimont <thomas.darimont@gmail.com>
2018-08-23 21:59:01 +0200

    Use latest stable keycloak helm chart

--- code/keycloak-helm/create-keycloak-cluster.sh
+++ code/keycloak-helm/create-keycloak-cluster.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#helm install --name keycloak --set keycloak.password=keycloak -f values.yml stable/keycloak
-helm install --name keycloak --set keycloak.password=keycloak -f values.yml /home/tom/dev/repos/gh/thomasdarimont/kubernetes-dev/charts/stable/keycloak
+helm install --name keycloak --set keycloak.password=keycloak -f values.yml stable/keycloak
+#helm install --name keycloak --set keycloak.password=keycloak -f values.yml /home/tom/dev/repos/gh/thomasdarimont/kubernetes-dev/charts/stable/keycloak
 
 kubectl apply -f keycloak-ingress.yaml
 
